53635d53d6586208a102b7d8fde32e0cdbb481c0
Aaryan <pes1202102130@pesu.pes.edu>
2023-04-29 23:12:11 +0530

    Added server code

---
+++ Server/Server.py
@@ -0,0 +1,108 @@
+import socket
+import threading
+import os
+
+HEADER = 64
+PORT = 5072
+SIZE=1024
+FORMAT = 'utf-8'
+DISCONNECT = "!DISCONNECT"
+SERVER = "0.0.0.0"
+ADDR = (SERVER, PORT)
+FINAL=""
+print(socket.gethostname())
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    connected = True
+    while connected:
+        global msg_length 
+
+        try:
+            msg_length= conn.recv(HEADER).decode(FORMAT)
+        except:
+            pass
+        else:
+
+        
+            if msg_length:
+                
+                #print(msg_length)
+                try:
+                    msg = conn.recv(int(msg_length)).decode(FORMAT)
+                except:
+                    send(conn)
+                else:
+
+                    if msg == "!DISCONNECT":
+                        connected = False
+                        print(f"[{addr}] {msg}")
+                
+                    elif os.path.isfile(msg):
+                        try:
+                            with open(msg, 'rb') as file:
+                                file_data = file.read()
+                                conn.send(file_data)
+                                print("File sent")
+                        except IOError:
+                            conn.send("Invalid Filename".encode(FORMAT))
+                    else:
+                        conn.send("Invalid Filename".encode(FORMAT))
+    
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        path = os.path.abspath(__file__)
+        path=path[-len(path):-9]
+        print(path)
+
+
+
+        files = os.listdir(path)
+        si=""
+        for file in files:
+            print(file)
+            si=si+file+"\n"
+        FINAL = f"{si}"
+        conn.send(FINAL.encode())
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+def send(conn):
+    
+    data = conn.recv(SIZE).decode(FORMAT)
+
+    item = data.split("_")
+    hello = item[0]
+    data=msg_length
+    item = data.split("_")
+    FILENAME=item[0]
+    print(FILENAME)
+    FILESIZE = int(item[1])
+ 
+    print("[+] Filename and filesize received from the client.")
+    #conn.send("Filename and filesize received".encode(FORMAT))
+    with open(f"{FILENAME}", "w") as f:
+        
+        #data = conn.recv(SIZE).decode(FORMAT)
+        #print("FILE CONTENT",data)
+        f.write(hello)
+        print("Done writing")
+        f.close()
+        
+ 
+            
+    
+    #conn.close()
+    #server.close()
+
+print("Server is now listening...")
+start()
